58ddad59e774aae4ac4158586aca8d83474a1bd1
Rabindra Tamang <rabindratamang147@gmail.com>
2020-05-19 00:35:44 +0545

    Select console error fix

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Countries = ({countries, selectedCountry, onCountrySelect}) => {
     return (
         <div className='country-select'>
-            <label for="countrySelect">Select Country</label>
-            <select className="form-control" id="countrySelect" onChange={(event)=> onCountrySelect(event)}>
+            <label htmlFor="countrySelect">Select Country</label>
+            <select className="form-control" id="countrySelect" defaultValue={selectedCountry} onChange={(event)=> onCountrySelect(event)}>
                 <option value="all">All</option>
                 {countries.map((country, index) => {
-                    return <option key={index} selected={selectedCountry === country.iso2} value={country.iso2}>{country.name}</option>
+                    return <option key={index} value={country.iso2}>{country.name}</option>
                 })}
             </select>
         </div>
