75820c1f525d4e143692879a470f83974aded9e7
Rabindra Tamang <rabindratamang147@gmail.com>
2020-05-19 13:31:47 +0545

    Add active cases in Stat chart

--- src/Components/Chart/StatChart.js
+++ src/Components/Chart/StatChart.js
@@ -9,17 +9,19 @@ const ChartWrapper = styled.div`
 
 const StatChart = ({stats}) => {
     const {confirmed, recovered, deaths} = stats;
+    const activeCases = {value: (confirmed.value - (recovered.value + deaths.value))};
+
     return <ChartWrapper>
                     <Doughnut
                     data={{
                         datasets: [
                         {
-                            data: [confirmed?.value, deaths?.value, recovered?.value],
-                            backgroundColor: ['#0088FE', '#dc6435', '#3cde61'],
-                            hoverBackgroundColor: ["#469efd", "#e08663",'#78f194' ],
+                            data: [activeCases?.value, deaths?.value, recovered?.value],
+                            backgroundColor: ['#ffc107', '#dc6435', '#3cde61'],
+                            hoverBackgroundColor: ["#fbce46", "#e08663",'#78f194' ],
                         },
                         ],
-                        labels: ["Confirmed", "Deaths", "Recovered"],
+                        labels: ["Active Cases", "Deaths", "Recovered"],
                     }}
                     width={160}
                     height={256}
