3af47d41e1a5b1f55c1687e6664273f74e800d8d
Rabindra Tamang <rabindratamang147@gmail.com>
2020-05-19 14:50:55 +0545

    Minor fix

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -3,7 +3,7 @@ import NumberFormat from "react-number-format";
 
 const Card = ({ stat, title, color }) => {
   return (
-    <div className="p-0 col-6 col-md-6 col-lg-3">
+    <div className="p-1 col-6 col-md-6 col-lg-3">
       <div className="card">
         <div className="card-body">
           <h1>{title}</h1>
