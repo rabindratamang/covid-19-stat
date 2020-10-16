303507daa771fcddb8f1ee131d38e0515827c3fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-16 07:51:43 +0000

    Deploy rabindratamang/covid-19-stat to github.com/rabindratamang/covid-19-stat.git:gh-pages

---
+++ precache-manifest.646f2277655dece587a1df85ce6c5298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0bf202e4c335660d49c3e6d4c9a544",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e085a480da700648018c",
+    "url": "./static/css/main.f51c4269.chunk.css"
+  },
+  {
+    "revision": "a3d6e313ebb8a1e5115e",
+    "url": "./static/js/2.4b32e563.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "./static/js/2.4b32e563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e085a480da700648018c",
+    "url": "./static/js/main.2bfe56df.chunk.js"
+  },
+  {
+    "revision": "d12e1992d11bb3ff91e4",
+    "url": "./static/js/runtime-main.04a8e1de.js"
+  }
+]);
